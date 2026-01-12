feca21c1a94bcf32b27a990f493d60b9f33b48d3
omar <omarqais225@gmail.com>
2026-01-12 18:43:56 +0200

    allow server website

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.SERVER,
     credentials: true
 }));
 app.use(express.json());
